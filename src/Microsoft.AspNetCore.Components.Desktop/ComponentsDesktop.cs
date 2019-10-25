8ca2254eb49649071be4d600fa2ff4b5410af8f7
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2019-10-25 09:49:31 +0300

    Refactor

--- src/Microsoft.AspNetCore.Components.Desktop/ComponentsDesktop.cs
+++ src/Microsoft.AspNetCore.Components.Desktop/ComponentsDesktop.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.JSInterop.Infrastructure;
+using System;
+using System.Diagnostics;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -7,6 +10,10 @@ namespace Microsoft.AspNetCore.Components.Desktop
 {
     public static class ComponentsDesktop
     {
+        internal static string InitialUriAbsolute { get; private set; }
+        internal static string BaseUriAbsolute { get; private set; }
+        internal static DesktopJSRuntime DesktopJSRuntime { get; private set; }
+
         public static void Run<TStartup>(string hostHtmlPath, Action<Form> configure = null)
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
@@ -18,10 +25,102 @@ namespace Microsoft.AspNetCore.Components.Desktop
 
             Task.Factory.StartNew(async() =>
             {
-                await form.RunAsync(hostHtmlPath);
+                var ipc = form.CreateChannel(hostHtmlPath);
+                await RunAsync(ipc);
             }, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext());
 
             Application.Run(form);
+        }
+
+        private static async Task RunAsync(IPC ipc)
+        {
+            DesktopJSRuntime = new DesktopJSRuntime(ipc);
+            await PerformHandshakeAsync(ipc);
+            AttachJsInterop(ipc);
+        }
+
+        private static async Task PerformHandshakeAsync(IPC ipc)
+        {
+            // Do a two-way handshake with the browser, this ensures that the appropriate
+            // interop handlers have been set up before control returns to the user.
+            //
+            // The handshake sequence looks like this:
+            //
+            // 1. dotnet starts listening for components:init
+            // 2. dotnet sends components:init repeatedly
+            // 3. electron starts listening for components:init
+            // 4. electron sends a components:init once it has received one from dotnet - it's ready
+            // 5. dotnet receives components:init - it's ready
+            //
+            // Because either side might take any amount of time to start listening,
+            // step 3 can occur at any point prior to step 4. The whole process works
+            // because steps 1, 2, 4, and 5 can only occur in that order
+
+            var cts = new CancellationTokenSource();
+            var incomingHandshakeCancellationToken = cts.Token;
+            ipc.Once("components:init", args =>
+            {
+                var argsArray = (object[])args;
+                InitialUriAbsolute = ((JsonElement)argsArray[0]).GetString();
+                BaseUriAbsolute = ((JsonElement)argsArray[1]).GetString();
+                cts.Cancel();
+            });
+
+            Log("Waiting for interop connection");
+            while (!incomingHandshakeCancellationToken.IsCancellationRequested)
+            {
+                ipc.Send("components:init");
+
+                try
+                {
+                    await Task.Delay(100, incomingHandshakeCancellationToken);
+                }
+                catch (TaskCanceledException)
+                {
+                }
+            }
+
+            Log("Interop connected");
+        }
+
+        private static void AttachJsInterop(IPC ipc)
+        {
+            var desktopSynchronizationContext = new DesktopSynchronizationContext();
+
+            SynchronizationContext.SetSynchronizationContext(desktopSynchronizationContext);
+
+            ipc.On("BeginInvokeDotNetFromJS", args =>
+            {
+                desktopSynchronizationContext.Send(state =>
+                {
+                    var argsArray = (object[])state;
+                    DotNetDispatcher.BeginInvokeDotNet(
+                        DesktopJSRuntime,
+                        new DotNetInvocationInfo(
+                            assemblyName: (string)argsArray[1],
+                            methodIdentifier: (string)argsArray[2],
+                            dotNetObjectId: (long)argsArray[3],
+                            callId: (string)argsArray[0]),
+                        (string)argsArray[4]);
+                }, args);
+            });
+
+            ipc.On("EndInvokeJSFromDotNet", args =>
+            {
+                desktopSynchronizationContext.Send(state =>
+                {
+                    var argsArray = (object[])state;
+                    DotNetDispatcher.EndInvokeJS(
+                        DesktopJSRuntime,
+                        (string)argsArray[2]);
+                }, args);
+            });
+        }
+
+        private static void Log(string message)
+        {
+            var process = Process.GetCurrentProcess();
+            Console.WriteLine($"[{process.ProcessName}:{process.Id}] out: " + message);
         }
     }
 }
--- src/Microsoft.AspNetCore.Components.Desktop/RootForm.cs
+++ src/Microsoft.AspNetCore.Components.Desktop/RootForm.cs
@@ -1,33 +1,22 @@
-﻿using Microsoft.JSInterop.Infrastructure;
-using Microsoft.Toolkit.Forms.UI.Controls;
+﻿using Microsoft.Toolkit.Forms.UI.Controls;
 using System;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Microsoft.AspNetCore.Components.Desktop
 {
     internal partial class RootForm : Form
     {
-        internal static string InitialUriAbsolute { get; private set; }
-        internal static string BaseUriAbsolute { get; private set; }
-        internal static DesktopJSRuntime DesktopJSRuntime { get; private set; }
-
         public RootForm()
         {
             InitializeComponent();
         }
 
-        public async Task RunAsync(string hostHtmlPath)
+        public IPC CreateChannel(string hostHtmlPath)
         {
             var webView = CreateWindow(hostHtmlPath);
             var ipc = new IPC(webView);
-            DesktopJSRuntime = new DesktopJSRuntime(ipc);
-            await PerformHandshakeAsync(ipc);
-            AttachJsInterop(ipc);
+            return ipc;
         }
 
         private WebView CreateWindow(string hostHtmlPath)
@@ -40,90 +29,6 @@ namespace Microsoft.AspNetCore.Components.Desktop
             wvc.IsScriptNotifyAllowed = true;
             wvc.NavigateToLocalStreamUri(new Uri(hostHtmlPath, UriKind.Relative), new ContentRootResolver());
             return wvc;
-        }
-
-        private async Task PerformHandshakeAsync(IPC ipc)
-        {
-            // Do a two-way handshake with the browser, this ensures that the appropriate
-            // interop handlers have been set up before control returns to the user.
-            //
-            // The handshake sequence looks like this:
-            //
-            // 1. dotnet starts listening for components:init
-            // 2. dotnet sends components:init repeatedly
-            // 3. electron starts listening for components:init
-            // 4. electron sends a components:init once it has received one from dotnet - it's ready
-            // 5. dotnet receives components:init - it's ready
-            //
-            // Because either side might take any amount of time to start listening,
-            // step 3 can occur at any point prior to step 4. The whole process works
-            // because steps 1, 2, 4, and 5 can only occur in that order
-
-            var cts = new CancellationTokenSource();
-            var incomingHandshakeCancellationToken = cts.Token;
-            ipc.Once("components:init", args =>
-            {
-                var argsArray = (object[])args;
-                InitialUriAbsolute = ((JsonElement)argsArray[0]).GetString();
-                BaseUriAbsolute = ((JsonElement)argsArray[1]).GetString();
-                cts.Cancel();
-            });
-
-            Log("Waiting for interop connection");
-            while (!incomingHandshakeCancellationToken.IsCancellationRequested)
-            {
-                ipc.Send("components:init");
-
-                try
-                {
-                    await Task.Delay(100, incomingHandshakeCancellationToken);
-                }
-                catch (TaskCanceledException)
-                {
-                }
-            }
-
-            Log("Interop connected");
-        }
-
-        private void AttachJsInterop(IPC ipc)
-        {
-            var desktopSynchronizationContext = new DesktopSynchronizationContext();
-
-            SynchronizationContext.SetSynchronizationContext(desktopSynchronizationContext);
-
-            ipc.On("BeginInvokeDotNetFromJS", args =>
-            {
-                desktopSynchronizationContext.Send(state =>
-                {
-                    var argsArray = (object[])state;
-                    DotNetDispatcher.BeginInvokeDotNet(
-                        DesktopJSRuntime,
-                        new DotNetInvocationInfo(
-                            assemblyName: (string)argsArray[1],
-                            methodIdentifier: (string)argsArray[2],
-                            dotNetObjectId: (long)argsArray[3],
-                            callId: (string)argsArray[0]),
-                        (string)argsArray[4]);
-                }, args);
-            });
-
-            ipc.On("EndInvokeJSFromDotNet", args =>
-            {
-                desktopSynchronizationContext.Send(state =>
-                {
-                    var argsArray = (object[])state;
-                    DotNetDispatcher.EndInvokeJS(
-                        DesktopJSRuntime,
-                        (string)argsArray[2]);
-                }, args);
-            });
-        }
-
-        private static void Log(string message)
-        {
-            var process = Process.GetCurrentProcess();
-            Console.WriteLine($"[{process.ProcessName}:{process.Id}] out: " + message);
         }
     }
 }
